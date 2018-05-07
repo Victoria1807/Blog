d3ca5d6082895be0fea3845b47b53018034eafb4
Victoria1807 <flamaster1807@gmail.com>
2018-05-07 17:19:00 +0300

    Added post delete view && maded template inheritance

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -13,8 +13,6 @@ def post_create(request):
         # message success
         messages.success(request, "Successfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
-    else:
-        messages.error(request, "Not successfully created")
     context = {
         "form": form,
     }
@@ -34,7 +32,7 @@ def post_list(request):
         "objects_list": queryset,
         "title": "List",
     }
-    return render(request, "index.html", context)
+    return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -52,5 +50,8 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete!</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully deleted")
+    return redirect("posts:list")
