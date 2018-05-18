21e83403bdd80bf4d1b09061ed64516cb81d39dc
Victoria1807 <flamaster1807@gmail.com>
2018-05-18 16:03:12 +0300

    Revert "Added draft and publish fields to post model"
    This reverts commit 2f2b4197cbfbb8bd2cbbb9ed286b7fb147f009c6.

--- posts/forms.py
+++ posts/forms.py
@@ -8,7 +8,5 @@ class PostForm(forms.ModelForm):
         fields = [
             "title",
             "content",
-            "image",
-            "draft",
-            "publish",
+            "image"
         ]
