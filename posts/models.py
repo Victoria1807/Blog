43a1574dcaf120e7d976f1ed097a62d745372cb7
Victoria1807 <flamaster1807@gmail.com>
2018-05-18 15:42:10 +0300

    Added user asociation with posts && added Facebook comments and likes

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
@@ -10,6 +11,7 @@ def upload_location(instance, filename):
     return "%s/%s" % (instance.id, filename)
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,
--- posts/views.py
+++ posts/views.py
@@ -9,9 +9,16 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
+    if not request.user.is_authenticated():
+        raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         # message success
         messages.success(request, "Successfully created")
