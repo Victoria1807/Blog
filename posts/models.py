116c59a9c3d0e86bd9b5ce17de1f357bad3040a7
Victoria1807 <flamaster1807@gmail.com>
2018-06-22 15:51:38 +0300

    Added comment model manager

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ from django.utils.text import slugify
 from django.utils import timezone
 
 from markdown_deux import markdown
+from comments.models import Comment
 
 # Create your models here.
 
@@ -50,6 +51,12 @@ class Post(models.Model):
         content = self.content
         markdown_text = markdown(content)
         return mark_safe(markdown_text)
+
+    @property
+    def comments(self):
+        instance = self
+        qs = Comment.objects.filter_by_instance(instance)
+        return qs
 
 
 def create_slug(instance, new_slug=None):
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,6 @@
 from urllib.parse import quote_plus
 
 from django.contrib import messages
-from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404
@@ -38,11 +37,7 @@ def post_detail(request, slug=None):
         if not request.user.is_staff or not request.user.is_superuser:
             raise Http404
     share_string = quote_plus(instance.content)
-    content_type = ContentType.objects.get_for_model(Post)
-    obj_id = instance.id
-    #Post.objects.get(id=instance.id)
-    comments = Comment.objects.filter(content_type=content_type, object_id=obj_id)
-
+    comments = instance.comments
     context = {
         "title": instance.title,
         "instance": instance,
