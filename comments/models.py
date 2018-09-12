3cae17f9b649946fed12fc7b9e7a2bb14288ed3f
Ostap Pankevych <flamaster1807@gmail.com>
2018-09-12 21:37:50 +0300

    Added comment thread

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
 
 from django.db import models
 
@@ -33,6 +34,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+    def get_absolute_url(self):
+        return reverse("comments:thread", kwargs={"id": self.id})
 
     def children(self): # replies
         return Comment.objects.filter(parent=self)
